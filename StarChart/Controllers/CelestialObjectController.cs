cf84846363bf2cc5d5f518051b17e5c6308f7772
Ricardo Esqueda Guerrero <RICARDOES@iadb.org>
2019-08-28 11:56:17 -0400

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -82,7 +82,7 @@ namespace StarChart.Controllers
             return NoContent();
         }
 
-        [HttpPatch("{id/name}")]
+        [HttpPatch("{id}/{name}")]
         public IActionResult RenameObject(int id, string name)
         {
             var existingObject = _context.CelestialObjects.Find(id);
