397cfa9b082bcc6e2708a4cf7de46287ad984f70
Ricardo Esqueda Guerrero <RICARDOES@iadb.org>
2019-08-28 11:30:28 -0400

    Completed the module

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    public class CelestialObjectController : ControllerBase
+    {
+        public CelestialObjectController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        private readonly ApplicationDbContext _context;
+    }
+}
